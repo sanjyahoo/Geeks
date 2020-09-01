508b1a85e90688a0e43815f3bc072333b124ecfd
sanjay yahoo <sjosjoysos@gmail.com>
2020-09-02 01:28:53 +0530

    adding space for PR

---
+++ src/StringManipulation/SubsStringCount.java
@@ -0,0 +1,21 @@
+package StringManipulation;
+
+
+//Tsys online test question: How to find total count of given substring in given string?
+//Input string: "welcomememe to hellome world"
+//Given substring: "me"
+//Output: total count of "me"
+
+
+public class SubsStringCount {
+
+	void countSubstring(String toFind, String inputString){
+		for(int i = 0;i<inputString.length();i=i++){
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
